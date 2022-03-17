54293c3b6fcdb37ef801f9a50277e288f3843824
Unknown <danila.vpopov@gmail.com>
2022-03-16 21:13:46 -0700

    doublespeak

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,27 @@
-* `department` * `id`: `INT PRIMARY KEY` * `name`: `VARCHAR(30)` to hold department name * `role` * `id`: `INT PRIMARY KEY` * `title`: `VARCHAR(30)` to hold role title * `salary`: `DECIMAL` to hold role salary * `department_id`: `INT` to hold reference to department role belongs to * `employee` * `id`: `INT PRIMARY KEY` * `first_name`: `VARCHAR(30)` to hold employee first name * `last_name`: `VARCHAR(30)` to hold employee last name * `role_id`: `INT` to hold reference to employee role * `manager_id`: `INT`
+DROP DATABASE IF EXISTS winston_db;
+CREATE DATABASE winston_db;
+USE winston_db;
+CREATE TABLE `department` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(30) NOT NULL UNIQUE,
+    PRIMARY KEY (`id`)
+);
+CREATE TABLE `role` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(30) NOT NULL UNIQUE,
+    `salary` DECIMAL NOT NULL,
+    `department_id` INT NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`department_id`) REFERENCES `department`(`id`) ON DELETE CASCADE
+);
+CREATE TABLE `employee` (
+    `id` INT AUTO_INCREMENT NOT NULL,
+    `first_name` VARCHAR(30) NOT NULL,
+    `last_name` VARCHAR(30) NOT NULL,
+    `role_id` INT,
+    `manager_id` INT,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`role_id`) REFERENCES `role`(`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`manager_id`) REFERENCES employee(`id`) ON DELETE
+    set null
+);
