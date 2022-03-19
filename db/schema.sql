3f953b3c4f288a580a579460192fbe9b967014ff
Unknown <danila.vpopov@gmail.com>
2022-03-18 22:05:26 -0700

    databases are ready

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
-DROP DATABASE IF EXISTS winston_db;
-CREATE DATABASE winston_db;
-USE winston_db;
+DROP DATABASE IF EXISTS management_db;
+CREATE DATABASE management_db;
+USE management_db;
 CREATE TABLE `department` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(30) NOT NULL UNIQUE,
